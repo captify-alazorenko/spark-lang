570cf8a0ff62bba7f61b6632c090bb40fe2b3b8e
andriy.lazorenko <andriy.lazorenko@captify.co.uk>
2017-03-24 10:48:10 +0200

    v. 0.1

--- postprocess_json.py
+++ postprocess_json.py
@@ -1,4 +1,17 @@
+import json
+
 path = '/home/andriy/Code/IdeaProjects/language-detection/profiles_9/en.json'
+common_path = '/home/andriy/Code/IdeaProjects/language-detection/profiles_9/'
+langs = ['en', 'de', 'fr']
+
+paths = [common_path + lan + 'json' for lan in langs]
 
 with open(path, 'r') as fh:
-    fh.read()
+    profile = json.load(fh)
+print(profile['a'])
+
+counts = dict()
+for key in profile:
+    if len(key) == 1:
+
+
